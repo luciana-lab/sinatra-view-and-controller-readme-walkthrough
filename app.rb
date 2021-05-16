d0aaeee5e354b4fab676d5bc5b1528f485614fc4
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-16 11:42:28 -0400

    commit tree 35f0ad970972718a11acde8eb5ddd686847cd5e3

--- app.rb
+++ app.rb
@@ -5,12 +5,19 @@ class App < Sinatra::Base
     erb :reverse
   end
 
+  # responds with a 200
+  # displays the reversed string
   post '/reverse' do
+    original_string = params[:string]
+    @reversed_string = original_string.reverse
     erb :reversed
   end
 
+  # responds with a 200
+  # displays friends in the view
   get '/friends' do
     # Write your code here!
-
+    @friends = ["Emily Wilding Davison", "Harriet Tubman", "Joan of Arc", "Malala Yousafzai", "Sojourner Truth"]
+    erb :friends
   end
 end
